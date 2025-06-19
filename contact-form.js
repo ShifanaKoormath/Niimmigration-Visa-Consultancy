2433ee0a198e2d6c5c60d9e8b6748d3fd7bbe3bd
Shifana K <shifanakoormath11@gmail.com>
2025-06-19 12:26:55 +0530

    Updated header CSS for responsiveness and improved visibility

--- contact-form.js
+++ contact-form.js
@@ -1,12 +1,12 @@
 // Initialize EmailJS with your Public Key
-emailjs.init("Yncjpb6MM_08Uq_Gt"); // Replace with your actual public key
+emailjs.init("YussyZXyTv_TXcGGHB"); // Replace with your actual public key
 
 // Handle form submission
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent the default form submission behavior
 
     // Send form data to EmailJS
-    emailjs.sendForm('service_qzfkruj', 'template_0ei53vr', this)
+    emailjs.sendForm('service_bk6ft2f', 'template_0ei53vr', this)
         .then(function(response) {
             alert('Submission Successful!');
         }, function(error) {
